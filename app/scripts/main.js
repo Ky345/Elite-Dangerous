398ad0cfb0e170ba4159bb8df13ca9e866293532
Kyle <kyle@elykinnovation.com>
2017-04-22 16:27:56 -0400

    Built remaining pages.  Chopped up HQ version of the ship model videos and assigned them to their respective pages. Added nav bar for navigating between pages.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,65 @@
 $(function(){
     $('#fullpage').fullpage({
-      anchors:['firstPage', 'secondPage', 'thirdPage']
+      anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+      onLeave: function(index, nextIndex, direction){
+        if(index == 1 && nextIndex > 1) {
+          $('#intro').remove();
+          $('#menu').animate({
+            opacity: '+1'
+          }, 500);
+        }
+        else if(index > 1 && nextIndex == 1) {
+          $('#menu').animate({
+            opacity: '-1'
+          }, 500);
+        }
+        if(nextIndex == 2) {
+          var newVideo = $('<div class="fullscreen-bg" id="idle" style="z-index: -2;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Sequence2.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').delay(500).remove();
+          },500);
+        }
+
+
+      },
+      onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+        if(nextSlideIndex == 1) {
+          var newVideo = $('<div class="fullscreen-bg" id="hangarVid" style="z-index: -3;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Sidewinder.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').remove();
+          },500);
+        }
+        if(nextSlideIndex == 2) {
+          var newVideo = $('<div class="fullscreen-bg" id="hangarVid" style="z-index: -3;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Anaconda.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').remove();
+          },500);
+        }
+        if(nextSlideIndex == 3) {
+          var newVideo = $('<div class="fullscreen-bg" id="hangarVid" style="z-index: -3;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Cobra.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').remove();
+          },500);
+        }
+        if(nextSlideIndex == 4) {
+          var newVideo = $('<div class="fullscreen-bg" id="hangarVid" style="z-index: -3;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Hauler.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').remove();
+          },500);
+        }
+        if(nextSlideIndex == 0) {
+          var newVideo = $('<div class="fullscreen-bg" id="idle" style="z-index: -2;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/Sequence2.mp4" type="video/mp4"></video></div>');
+          $('.fullscreen-bg').last().after(newVideo);
+          setTimeout(function() {
+            $('body').find('.fullscreen-bg:lt(1)').delay(500).remove();
+          },500);
+        }
+      }
     });
     $.fn.fullpage.setAllowScrolling(false);
     $.fn.fullpage.setKeyboardScrolling(false);
@@ -23,16 +82,6 @@ $(function(){
     $('#intro video').on('ended',function(){
       $('#intro').remove();
     }); 
-
-    $('#sidewinderLink').on('click',function(e){
-      var newVideo = $('<div class="fullscreen-bg" id="hangarVid" style="z-index: -3;"><video loop muted autoplay class="fullscreen-video"><source id="source" src="images/hangar.mp4" type="video/mp4"></video></div>');
-      $('.fullscreen-bg').last().after(newVideo);
-      setTimeout(function(){
-        $('#idle').remove();
-        $('#intro').remove();
-      },600);
-      var target = $('#sidewinder');
-    });
 
     
 });
